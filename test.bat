e4dc38ba9863e57394496b63435c18da0420391f
Daniil Troshkov <troshkovdanil@gmail.com>
2023-08-30 23:42:37 +0400

    test

--- test.bat
+++ test.bat
@@ -1,9 +1,13 @@
-if not exist "build" mkdir build
+if exist "build" del /S /Q build
+mkdir build
 cd build
 
 python ..\test\test.py
 
 cl ..\test\test.c /Fotest_c.obj /Fetest_c.exe
 test_c.exe
+
+cl ..\test\test.cpp /Fotest_cpp.obj /Fetest_cpp.exe /EHsc
+test_cpp.exe
 
 cd ..
