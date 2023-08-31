65ba3b13a02ab7d9a6803673ce638914deb3c389
Daniil Troshkov <troshkovdanil@gmail.com>
2023-08-31 19:53:17 +0400

    test

--- test/test.py
+++ test/test.py
@@ -16,14 +16,17 @@ print(f"{bcolors.OKGREEN}Hi Python!{bcolors.ENDC}")
 import pytest
 
 
-def test_dec_10111():
+def test_dec_bin_10111():
     assert int("10111", 2) == 23
 
-def test_dec_11001010():
+def test_dec_bin_11001010():
     assert int("11001010", 2) == 202
 
-def test_bin_42():
+def test_bin_dec_42():
     assert str(bin(42)) == "0b101010"
 
-def test_bin_495():
+def test_bin_dec_495():
     assert str(bin(495)) == "0b111101111"
+
+def test_dec_hex_3B7C():
+    assert int("3B7C", 16) == 15228
